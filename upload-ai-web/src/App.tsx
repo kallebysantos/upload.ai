95861a11849b2b34f36a27832750f279b8f6f5e3
kallebysantos <kalleby_santos@hotmail.com>
2023-10-17 12:36:43 +0100

    📦 NEW: Adding shadcn/ui

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
     <>
       <div>
         <h1>Upload-AI</h1>
+
+        <Button>Teste</Button>
       </div>
     </>
   )
